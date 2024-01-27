db11e174d6a298fb53944dc68a587f6d0d0a8000
nicolas <n.s.borego@gmail.com>
2024-01-27 20:59:07 -0300

    even-or-odd

---
+++ csharp/even-or-odd/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Solution
+{
+  public class SolutionClass
+  {
+    public static string EvenOrOdd(int number) => (number % 2 == 0) ? "Even" : "Odd";
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine(EvenOrOdd(2));
+    }
+  }
+
+
+}
