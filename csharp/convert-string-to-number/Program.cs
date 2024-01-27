2d4cefa0facb2a22d7527c4b93874cf6f07b8701
nicolas <n.s.borego@gmail.com>
2024-01-27 10:53:02 -0300

    convert string to number

---
+++ csharp/convert-string-to-number/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Kata
+{
+  public static int StringToNumber(String str) => int.Parse(str);
+
+  static void Main(string[] args)
+  {
+    Console.WriteLine(StringToNumber("1234"));
+  }
+}
