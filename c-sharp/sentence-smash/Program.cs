29cc9cdeaddca0595c81bbbed90d2dde8326f749
nicolas <n.s.borego@gmail.com>
2024-01-26 23:13:10 -0300

    desafios c-sharp

---
+++ c-sharp/sentence-smash/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Kata
+{
+  public static string Smash(string[] words) => string.Join(" ", words);
+
+  static void Main(string[] args)
+  {
+    Console.Write(Smash(new string[] { "hello", "world", "this", "is", "great" }));
+  }
+}
+
+
