89545c3eed7289acbdcb1521a23d3b8d40a7d04e
nicolas <n.s.borego@gmail.com>
2024-01-27 11:07:01 -0300

    make to uppercase

---
+++ csharp/make-to-uppercase/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Kata
+{
+  public static string MakeUpperCase(string str) => str.ToUpper();
+
+  static void Main(string[] args)
+  {
+    Console.WriteLine(MakeUpperCase("str to uppercase"));
+  }
+}
